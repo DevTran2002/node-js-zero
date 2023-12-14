e98b085c6abf4c57e671a5e7af74f5307c62bdfd
Tran Ngoi <tranngoihdnv@gmail.com>
2023-12-14 13:02:37 +0700

    update

--- src/app.js
+++ src/app.js
@@ -4,10 +4,12 @@ const app = express()
 require('dotenv').config()
 const port = process.env.PORT
 
+//config template engine
 app.set('views' , path.join(__dirname , 'views'))
 app.set('view engine', 'ejs')
-
-
+//config static files
+app.use(express.static(path.join(__dirname , 'public')))
+//khai báo route
 app.get('/', (req, res) => {
   res.render('sample.ejs')
 })
